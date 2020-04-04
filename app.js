481959b8ae932d93f20d36b7859f957b189c4373
surazzShahil <32029837+Surazzshahil@users.noreply.github.com>
2020-04-04 14:11:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+function covid19(country){
+        // // proxy for local server
+        let proxy = 'https://cors-anywhere.herokuapp.com/';
+        // covid-19 api link
+        let covid19 = "http://covid-rest.herokuapp.com/";
+        let api = `${proxy}${covid19}${country}`;
+        // DOM selector
+
+        fetch(api,{
+            "method":"GET"
+        })
+        .then(response=>{
+            return response.json();
+        })
+        .then(data=>{
+            var err = document.querySelector('.error.message');
+            console.log(data);
+            if(data.code =="404"){
+                err.innerHTML = "country not found";
+            }else {
+                err.innerHTML ="";
+                // select ------------------------------------
+            let countryDom = document.querySelector('.country');
+            let cases = document.querySelector('#cases');
+            let death = document.querySelector('#death');
+            let recovered = document.querySelector('#recovered');
+            let active = document.querySelector('#active');
+            // execute------------------------------------
+            countryDom.innerHTML= data.data.country_name;
+            cases.innerHTML = data.data.total_cases;
+            death.innerHTML = data.data.total_death;
+            recovered.innerHTML = data.data.total_recovered;
+            active.innerHTML = data.data.active_cases;
+            }
+            
+        })
+}
+covid19('india');
+let input = document.querySelector('input');
+let submitbtn = document.querySelector('button');
+window.onload = function(){
+    submitbtn.addEventListener('click',()=>{
+        let countryInp = input.value;
+        covid19(countryInp);
+    })
+}
+
+
+
+
+
+
+
